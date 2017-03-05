48c07a95a87c1da0c6c12595905ab96f2d1cd0cb
Arnold Foster <arnoldfoster@Arnolds-MacBook-Pro.local>
2017-03-05 17:20:58 +0000

    Remove gig date duplicate

--- src/Pages/stores/GigData.js
+++ src/Pages/stores/GigData.js
@@ -5,10 +5,6 @@ export const gigs = [
         date: '28th May 2017',
         address: '17-19 Wallgate, Wigan WN1 1LD'
     },
-    {
-        venue: 'The Boulevard',
-        date: '28th May 2017',
-        address: '17-19 Wallgate, Wigan WN1 1LD'
-    },
+
 
 ];
