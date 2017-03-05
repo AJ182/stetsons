5dea1b4828be4aa563cfac4d32f46f888c0eaeda
Arnold Foster <arnoldfoster@Arnolds-MacBook-Pro.local>
2017-03-05 21:00:15 +0000

    Fix margins for mobile

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -10,22 +10,22 @@ class About extends React.Component {
                   <div className="titles videosPageTitle">
                       About
                   </div>
-                  <div className="pageItem">
-                  <div className="site__headers">
+                  <div className="pageItem" style={{ margin: "0 20px" }}>
+                      <div className="site__headers">
 
-                      Rock n Roll from the heart of the Wild Northwest.
-                      <br/>
-                      Tommy Hewitt. Tommy Jones. The Prospector.
-                      <br/>
-                      "Keeps the Gold Clean and the mind keen"
-                  </div>
+                          Rock n Roll from the heart of the Wild Northwest.
+                          <br/>
+                          Tommy Hewitt. Tommy Jones. The Prospector.
+                          <br/>
+                          "Keeps the Gold Clean and the mind keen"
+                      </div>
 
-                  <div className="site__headers">
-                      The Stetsons Electric are Revolution Battle of the bands Winners, <br/>and now officially 'The definition of rock and roll' according to <br/>Lancaster's newspaper 'Scan'.
-                  </div>
-                  <div className="site__headers">
-                      From shows across the Northwest, to performing <br/>live on BBC Radio Manchester, The Stetsons Electric <br/>make a lasting impact wherever they go.
-                  </div>
+                      <div className="site__headers">
+                          The Stetsons Electric are Revolution Battle of the bands Winners, <br/>and now officially 'The definition of rock and roll' according to <br/>Lancaster's newspaper 'Scan'.
+                      </div>
+                      <div className="site__headers">
+                          From shows across the Northwest, to performing <br/>live on BBC Radio Manchester, The Stetsons Electric <br/>make a lasting impact wherever they go.
+                      </div>
                   </div>
               </GlobalLayout>
           </div>
