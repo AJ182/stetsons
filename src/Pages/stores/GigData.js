52016cbcfc7e15a57ef4ac0ee797b40bde8101d4
Arnold Foster <arnoldfoster@Arnolds-MacBook-Pro.local>
2017-09-09 17:01:33 +0100

    Remove old gigs, fix contact message width

--- src/Pages/stores/GigData.js
+++ src/Pages/stores/GigData.js
@@ -1,13 +1,8 @@
 
 export const gigs = [
     {
-        venue: 'The Eagle and Child',
-        date: '19th August 2017',
-        address: 'Billinge, WN5 7HD'
-    },
-    {
-        venue: 'Indiependence',
-        date: '27th August 2017',
-        address: '6 King St West, Wigan WN1 1LP'
+        venue: 'No Upcoming shows, contact us to book!',
+        date: 'To be announced',
+        address: 'Earth'
     },
 ];
